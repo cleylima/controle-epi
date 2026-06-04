78affad4f9ecc38e83ea3125b16657bc960adbf4
cleylima <cleyson.lima.pb@gmail.com>
2026-06-04 14:02:22 -0300

    CRUD completo de funcionarios

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,7 +1,35 @@
 from django.urls import path
-from .views import listar_funcionarios, novo_funcionario
+
+from .views import (
+    listar_funcionarios,
+    novo_funcionario,
+    editar_funcionario,
+    excluir_funcionario
+)
 
 urlpatterns = [
-    path('', listar_funcionarios, name='listar_funcionarios'),
-    path('novo/', novo_funcionario, name='novo_funcionario'),
+
+    path(
+        '',
+        listar_funcionarios,
+        name='listar_funcionarios'
+    ),
+
+    path(
+        'novo/',
+        novo_funcionario,
+        name='novo_funcionario'
+    ),
+
+    path(
+        'editar/<int:pk>/',
+        editar_funcionario,
+        name='editar_funcionario'
+    ),
+
+    path(
+        'excluir/<int:pk>/',
+        excluir_funcionario,
+        name='excluir_funcionario'
+    ),
 ]
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Funcionario
 from .forms import FuncionarioForm
+from django.shortcuts import render, redirect, get_object_or_404
 
 
 def listar_funcionarios(request):
@@ -35,4 +36,50 @@ def novo_funcionario(request):
         {
             'form': form
         }
+    )
+    
+def editar_funcionario(request, pk):
+
+    funcionario = get_object_or_404(
+        Funcionario,
+        pk=pk
+    )
+
+    if request.method == 'POST':
+
+        form = FuncionarioForm(
+            request.POST,
+            instance=funcionario
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_funcionarios')
+
+    else:
+
+        form = FuncionarioForm(
+            instance=funcionario
+        )
+
+    return render(
+        request,
+        'funcionarios/form.html',
+        {
+            'form': form
+        }
+    )
+
+
+def excluir_funcionario(request, pk):
+
+    funcionario = get_object_or_404(
+        Funcionario,
+        pk=pk
+    )
+
+    funcionario.delete()
+
+    return redirect(
+        'listar_funcionarios'
     )
