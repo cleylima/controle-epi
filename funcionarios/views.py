df95d12b1f28fc6d30ab6e5b627607cb15a40468
cleylima <cleyson.lima.pb@gmail.com>
2026-06-06 08:56:02 -0300

    feat: fica de EPI atualizada

--- funcionarios/views.py
+++ funcionarios/views.py
@@ -5,13 +5,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from entregas.models import EntregaEPI
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from datetime import date
 
 from reportlab.platypus import (
     SimpleDocTemplate,
     Paragraph,
-    Spacer
+    Spacer,
+    Table,
+    TableStyle,
 )
 
+from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from entregas.models import EntregaEPI
 
@@ -122,6 +126,9 @@ def historico_funcionario(request, pk):
 
 @login_required
 def gerar_ficha_epi_pdf(request, pk):
+    from reportlab.platypus import Image
+    from django.conf import settings
+    import os
 
     funcionario = get_object_or_404(
         Funcionario,
@@ -150,15 +157,39 @@ def gerar_ficha_epi_pdf(request, pk):
     styles = getSampleStyleSheet()
 
     elementos = []
+    
+    logo_path = os.path.join(
+        settings.BASE_DIR,
+        'static',
+        'img',
+        'logoecocristal.png'
+    )
+
+    if os.path.exists(logo_path):
+
+        logo = Image(
+            logo_path,
+            width=120,
+            height=60
+        )
+
+        elementos.append(logo)
+
+        elementos.append(
+            Spacer(1, 10)
+        )
+
 
     elementos.append(
         Paragraph(
-            'FICHA DE ENTREGA DE EPI',
-            styles['Title']
+            'FICHA DE ENTREGA DE EQUIPAMENTO DE PROTEÇÃO INDIVIDUAL',
+            styles['Heading2']
         )
     )
 
     elementos.append(Spacer(1, 20))
+
+    # Dados do funcionário
 
     elementos.append(
         Paragraph(
@@ -181,49 +212,79 @@ def gerar_ficha_epi_pdf(request, pk):
         )
     )
 
+    elementos.append(
+        Paragraph(
+            f'<b>Data de Emissão:</b> {date.today().strftime("%d/%m/%Y")}',
+            styles['Normal']
+        )
+    )
+
     elementos.append(Spacer(1, 20))
+
+    # Tabela
+
+    dados = [
+        ['EPI', 'Quantidade', 'Data Entrega']
+    ]
 
     for entrega in entregas:
 
-        elementos.append(
-            Paragraph(
-                f'''
-                {entrega.epi.nome}
-                - Qtd: {entrega.quantidade}
-                - Data: {entrega.data_entrega.strftime("%d/%m/%Y")}
-                ''',
-                styles['Normal']
-            )
-        )
+        dados.append([
+            entrega.epi.nome,
+            str(entrega.quantidade),
+            entrega.data_entrega.strftime('%d/%m/%Y')
+        ])
 
-    elementos.append(Spacer(1, 40))
+    tabela = Table(
+        dados,
+        colWidths=[250, 100, 120]
+    )
+
+    tabela.setStyle(
+        TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#D9EAD3')),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('ALIGN', (1, 1), (-1, -1), 'CENTER'),
+        ])
+    )
+
+    elementos.append(tabela)
+
+    elementos.append(Spacer(1, 30))
+
+    # Declaração
 
     elementos.append(
         Paragraph(
             '''
-            Declaro ter recebido os EPIs acima
-            e estar ciente da obrigatoriedade
-            de sua utilização.
+            Declaro ter recebido os equipamentos de proteção
+            individual descritos acima e estar ciente da
+            obrigatoriedade de sua utilização.
             ''',
             styles['Normal']
         )
     )
 
-    elementos.append(Spacer(1, 50))
+    elementos.append(Spacer(1, 60))
 
-    elementos.append(
-        Paragraph(
-            '__________________________________',
-            styles['Normal']
-        )
+    # Assinaturas
+
+    assinaturas = Table(
+        [
+            [
+                '________________________',
+                '________________________'
+            ],
+            [
+                'Colaborador',
+                'Responsável pela Entrega'
+            ]
+        ],
+        colWidths=[250, 250]
     )
 
-    elementos.append(
-        Paragraph(
-            'Assinatura do Colaborador',
-            styles['Normal']
-        )
-    )
+    elementos.append(assinaturas)
 
     doc.build(elementos)
 
